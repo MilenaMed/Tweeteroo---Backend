0ea130f7d3345c088c7262e7cf16244f9bff2d45
MilenaMed <milenamfm@gmail.com>
2023-06-26 16:29:16 -0300

    Armazenamento e formato dos dados
    Para persistir os dados (usuários e tweets), utilize variáveis globais em memória.

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+// POST - Criar usuário
+app.post("/sign-up", (request, response) => {
+    const novoUsuário =
+    {
+        username: request.body.username,
+        avatar: request.body.avatar
+    }
+
+    usuarios.push(novoUsuário)
+    response.send("OK")
+})
+
 const porta = process.env.PORT || 5000;
 app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
